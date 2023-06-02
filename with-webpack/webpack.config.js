5432ac996741d16a3aef3013322cba3c63860f92
Jennifer Bath <jennifer@rpmsoftware.com>
2023-06-02 14:11:08 -0600

    Demonstrate how to configure what webpack outputs
    - this example shows a custom directory name and and a custom file name for webpack to generate.

--- with-webpack/webpack.config.js
+++ with-webpack/webpack.config.js
@@ -1,6 +1,16 @@
+// Path is a build in node module that comes with Node.js
+// It enables developers to create file paths independent of the platform
+const path = require("path");
+
 module.exports = {
   mode: "development",
   // this rule makes main.js easier to read in development mode
   devtool: false,
   entry: "./src/newIndex.js",
+  output: {
+    //the default __dirname that webpack spits out is 'dist', however, this can be configured to be anything. In this case, 'build'.
+    path: path.resolve(__dirname, "build"),
+    // the default file name that webpack spits out is main.js, however, this can be configured. In this case, 'bundle.js'.
+    filename: "bundle.js",
+  },
 };
