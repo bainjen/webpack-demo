7b4ba7fa5b1b70704b8542e746a8b05bfeb586d9
Jennifer Bath <jennifer@rpmsoftware.com>
2023-06-02 14:25:54 -0600

    Fix react console warning by using correct import path

--- with-webpack/src/newIndex.js
+++ with-webpack/src/newIndex.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 // This code is telling React to insert React code into the element with the ID 'entryPointID'
 const container = document.getElementById("entryPointID");
