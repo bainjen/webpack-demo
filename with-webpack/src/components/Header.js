b81d99c841660d679201199156e6b348108aa1af
Jennifer Bath <jennifer@rpmsoftware.com>
2023-06-16 14:40:09 -0600

    Convert Header from vanilla JS to JSX
    - this was originally written in a way it could be read by the browser with out a transpiler
    - now that everything is being transpiled, it can be rendered using React JSX

--- with-webpack/src/components/Header.js
+++ with-webpack/src/components/Header.js
@@ -1,16 +1,35 @@
 import React from "react";
 
-const Header = (props) => {
-  const { links, ...rest } = props;
+// const Header = (props) => {
+//   const { links, ...rest } = props;
+//   const clickLinks = links.map((link, i) => {
+//     return React.createElement("a", { key: i, href: link.path }, link.name);
+//   });
+
+//   return React.createElement(
+//     "header",
+//     { ...rest },
+//     React.createElement("h2", null, "Donuts make me go nuts!"),
+//     React.createElement("nav", null, ...clickLinks)
+//   );
+// };
+
+// export default Header;
+
+const Header = ({ links, ...rest }) => {
   const clickLinks = links.map((link, i) => {
-    return React.createElement("a", { key: i, href: link.path }, link.name);
+    return (
+      <a key={i} href={link.path}>
+        {link.name}
+      </a>
+    );
   });
 
-  return React.createElement(
-    "header",
-    { ...rest },
-    React.createElement("h2", null, "Donuts make me go nuts!"),
-    React.createElement("nav", null, ...clickLinks)
+  return (
+    <header>
+      <h2>Donuts make me go nuts!</h2>
+      <nav>{clickLinks}</nav>
+    </header>
   );
 };
 
